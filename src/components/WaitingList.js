4cad07f477223317d460825a4e7937d829a85c40
ijw9209 <ijw9209@naver.com>
2020-04-07 02:14:05 +0900

    layout commit

---
+++ src/components/WaitingList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './WaitingList.css'
+
+const WaitingItem = ({ text, entered, onEnter, onLeave }) => {
+    return (
+        <li>
+            <div className={`text ${entered ? 'entered' : ''}`}>{text}</div>
+            <div className="buttons">
+                <button onClick={onEnter}>enter</button>
+                <button onClick={onLeave}>leave</button>
+            </div>
+        </li>
+    )
+}
+
+
+const WaitingList = ({ waitingList, onEnter, onLeave }) => {
+    return (
+        <div className="WaitingList">
+            <h2>대기자 명단</h2>
+            <form>
+                <input />
+                <button>등록</button>
+            </form>
+            <ul>
+                <WaitingItem text="홍길동" entered />
+                <WaitingItem text="콩쥐" />
+                <WaitingItem text="팥쥐" />
+            </ul>
+        </div>
+    );
+};
+
+export default WaitingList;
